c1d291c2d46311f071e8604a384582508decff48
余胜 <wx_a2f53f9499804ae094c7d8ca7817d653@git.code.tencent.com>
2019-12-23 09:24:41 +0800

    支持Android使用

--- android/src/main/java/com/zaixiaoqu/biglist/RnBigListModulePackage.java
+++ android/src/main/java/com/zaixiaoqu/biglist/RnBigListModulePackage.java
@@ -9,6 +9,7 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 import com.facebook.react.bridge.JavaScriptModule;
+import com.zaixiaoqu.biglist.component.SpringScrollViewManager;
 
 public class RnBigListModulePackage implements ReactPackage {
     @Override
@@ -18,6 +19,8 @@ public class RnBigListModulePackage implements ReactPackage {
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-        return Collections.emptyList();
+        return Arrays.<ViewManager>asList(
+                new SpringScrollViewManager()
+        );
     }
 }
---
+++ android/src/main/java/com/zaixiaoqu/biglist/component/Types.java
@@ -0,0 +1,19 @@
+package com.zaixiaoqu.biglist.component;
+
+class Offset {
+    public float x, y;
+
+}
+
+
+class EdgeInsets {
+    public float top, bottom, left, right;
+}
+
+class Size {
+    public float width, height;
+}
+
+class Point {
+    public float x, y;
+}
